590476cd7dae46e4a9a0930a08d21d20cd3486f5
Victor Antunez <victorac94@hotmail.com>
2019-03-21 16:04:15 +0100

    Updates

---
+++ precache-manifest.5015dbdf9fbdc0544ae552ff80645f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ccff9e5aa7249d1ac63e6445506cf65",
+    "url": "/crypto-tracker/static/media/Go Back.4ccff9e5.svg"
+  },
+  {
+    "revision": "feb83028dca41af3bd04",
+    "url": "/crypto-tracker/static/js/runtime~main.feb83028.js"
+  },
+  {
+    "revision": "1baf86cbc96f6010966d",
+    "url": "/crypto-tracker/static/js/main.1baf86cb.chunk.js"
+  },
+  {
+    "revision": "d6748e61120245457800",
+    "url": "/crypto-tracker/static/js/2.d6748e61.chunk.js"
+  },
+  {
+    "revision": "1baf86cbc96f6010966d",
+    "url": "/crypto-tracker/static/css/main.fa7cda53.chunk.css"
+  },
+  {
+    "revision": "03355bcd5c2c8f4d1dee908fec22c8bc",
+    "url": "/crypto-tracker/index.html"
+  }
+];
